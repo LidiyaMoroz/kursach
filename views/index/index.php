48fdfa3c0b3b7be6151e0443ace6e683d53892a6
LidiyaMoroz <lidiaymoroz1518@gmail.com>
2024-12-17 03:04:01 +0300

    add picture on start page

--- views/index/index.php
+++ views/index/index.php
@@ -1,1 +1,21 @@
-<?php
+<style>
+    .picture {
+        position: relative;
+        width: 400px;
+        height: 400px;
+    }
+    .picture img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        object-fit: cover;
+    }
+
+</style>
+<div class="container d-flex justify-content-center align-items-center">
+    <div class="picture">
+        <img src="/storage/icons/dog.svg" alt="logo">
+    </div>
+</div>
